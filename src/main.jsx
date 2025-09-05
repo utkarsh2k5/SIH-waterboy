2c05a3472452a06b2594354d49c7b39f9d9c6c86
utkarsh2k5 <utkarsh1601kumar@gmail.com>
2025-09-06 03:58:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.jsx';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Chatbot from './components/chatbot.jsx'; 
+import Chatbot from './components/Chatbot.jsx'; 
 
 const router = createBrowserRouter([
   {
